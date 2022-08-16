f7ab47fa17ab38bc1d311eb8d28977339830422c
Iam maria theresa <iam.mariatheresaelkazzi@gmail.com>
2022-08-16 20:41:12 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+const record = [
+    { year: "2018", result: "N/A" },
+    { year: "2017", result: "N/A" },
+    { year: "2016", result: "N/A" },
+    { year: "2015", result: "W" },
+    { year: "2014", result: "N/A" },
+    { year: "2013", result: "L" }
+]
+
+function superbowlWin(record) {
+    const res = record.find((record) =>
+        record.result === 'W'
+    );
+    return res ? res.year : undefined;
+
+}
+superbowlWin(record);
+
+
+
